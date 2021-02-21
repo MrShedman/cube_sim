01aa434024fcc6ade5b414c18a39825ed15cba36
Ross Cunningham <roscoacer@googlemail.com>
2021-02-21 19:23:33 +0000

    Simplifed grid

--- src/grid.py
+++ src/grid.py
@@ -8,9 +8,10 @@ from transform import Transform
 from mesh import Mesh
 
 class Grid(Transform):
-    def __init__(self, interval = 0.5):
+    def __init__(self, interval = 0.5, extent = 50.0):
         super().__init__()
         self.interval = interval
+        self.extent = extent
         self.count = 0
 
     def addVertex(self, x, y, z):
@@ -19,18 +20,17 @@ class Grid(Transform):
         self.count += 1
 
     def buildMesh(self):
-        extent = 100
-        segments, step = np.linspace(-extent, extent, int(extent / self.interval), False, True)
+        segments = np.linspace(-self.extent, self.extent, int((self.extent+0.5) / self.interval), endpoint=True)
 
-        self.mesh = Mesh(int(4 * step * len(segments)))
+        self.mesh = Mesh(4 * len(segments))
         self.mesh.mode = GL.GL_LINES
 
         for ix in segments:
-            self.addVertex(ix, -extent, 0.0)
-            self.addVertex(ix,  extent, 0.0)
+            self.addVertex(ix, -self.extent, 0.0)
+            self.addVertex(ix,  self.extent, 0.0)
 
         for iy in segments:
-            self.addVertex( extent, iy, 0.0)
-            self.addVertex(-extent, iy, 0.0)
-        
+            self.addVertex( self.extent, iy, 0.0)
+            self.addVertex(-self.extent, iy, 0.0)
+
         self.mesh.complete()
