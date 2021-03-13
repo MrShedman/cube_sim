4c7315056e3daa96ca9d55b6f8bbecf799f8e47e
Ross Cunningham <roscoacer@googlemail.com>
2021-03-13 23:38:21 +0000

    Big refactor to seperate game logic from cell surface logic. Should allow easy coding of more cell games

--- cube_sim/examples/endless_surface.py
+++ cube_sim/examples/endless_surface.py
@@ -11,136 +11,53 @@ import glm
 import math
 import random
 
-MAX_SPEED = 0.0075#15
+MAX_SPEED = 0.0075
 
-class Particle():
-    def __init__(self, dims):
-        self.dims = dims - 1
-        self.face = random.choice(list(Face))
-        self.pos = glm.vec2(random.uniform(0, self.dims), random.uniform(0, self.dims))
-        self.ipos = glm.ivec2(glm.floor(self.pos))
+class Cell():
+    def __init__(self, face, pos):
+        self.ipos = glm.ivec2(pos)
+        self.face = face
 
-        speedx = random.uniform(MAX_SPEED*0.2, MAX_SPEED)
-        speedy = random.uniform(MAX_SPEED*0.2, MAX_SPEED)
-        signx = (random.randint(0, 1) * 2) - 1
-        signy = (random.randint(0, 1) * 2) - 1
-        self.vel = glm.vec2(speedx * signx, speedy * signy)
+class MovingCell(Cell):
+    def __init__(self, face, pos, vel):
+        self.fpos = glm.vec2(pos)
+        self.vel = glm.vec2(vel)
+        super().__init__(face, glm.floor(self.fpos))
+
+class Snake(MovingCell):
+    def __init__(self, surface):
+        self.surface = surface
+        f = random.choice(list(Face))
+        p = glm.vec2(random.uniform(0, surface.dims), random.uniform(0, surface.dims))
+
+        speed = random.uniform(MAX_SPEED*0.2, MAX_SPEED)
+        direction = random.choice([-1, 1])
+        axis = random.choice([0, 1])
+        v = glm.vec2()
+        v[axis] = speed * direction
+
+        super().__init__(f, p, v)
         self.col = genRandomStrongColour()
-        self.trail = random.uniform(8, 64)
-        i = np.argmax(self.vel)
-        self.vel[i] = self.vel[i]
-        self.vel[i^1] = 0
-        self.pos_list = list()
-        self.addNewPos(self.face, self.pos)
+        self.length = random.uniform(2, 4)
+        self.tail = list()
 
-    def update(self, dt):
-        self.pos += self.vel / dt
-        ipos2 = glm.ivec2(glm.floor(self.pos))
-        add2list = False
-        if ipos2 != self.ipos:
-            add2list = True
-        self.ipos = ipos2
+    def step(self):
+        pos = glm.ivec2(self.fpos)
+        if len(self.tail) > 0:
+            frontCell = self.tail[0]
+            # something is causing duplicates to be added to the front triggering self intersection
+            if frontCell.face != self.face or frontCell.ipos != pos:
+                self.tail = [Cell(self.face, pos)] + self.tail   #add new at front
+                if len(self.tail) > self.length:
+                    self.tail = self.tail[:-1]      #remove last
+        else:
+            self.tail = [Cell(self.face, pos)] + self.tail   #add new at front
 
-        if (self.face == Face.BACK and self.ipos.x > self.dims):
-            self.face = Face.LEFT
-            self.pos = glm.vec2(0, self.ipos.y)
-        elif (self.face == Face.BACK and self.ipos.x < 0):
-            self.face = Face.RIGHT
-            self.pos = glm.vec2(self.dims, self.ipos.y)
-        elif (self.face == Face.BACK and self.ipos.y > self.dims):
-            self.face = Face.TOP
-            self.pos = glm.vec2(0, self.dims - self.ipos.x)
-            self.vel.x, self.vel.y = self.vel.y, -self.vel.x
-        elif (self.face == Face.BACK and self.ipos.y < 0):
-            self.face = Face.BOTTOM
-            self.pos = glm.vec2(0, self.ipos.x)
-            self.vel.x, self.vel.y = -self.vel.y, self.vel.x
-
-        elif (self.face == Face.FRONT and self.ipos.x > self.dims):
-            self.face = Face.RIGHT
-            self.pos = glm.vec2(0, self.ipos.y)
-        elif (self.face == Face.FRONT and self.ipos.x < 0):
-            self.face = Face.LEFT
-            self.pos = glm.vec2(self.dims, self.ipos.y)
-        elif (self.face == Face.FRONT and self.ipos.y > self.dims):
-            self.face = Face.TOP
-            self.pos = glm.vec2(self.dims, self.ipos.x)
-            self.vel.x, self.vel.y = -self.vel.y, self.vel.x
-        elif (self.face == Face.FRONT and self.ipos.y < 0):
-            self.face = Face.BOTTOM
-            self.pos = glm.vec2(self.dims, self.dims - self.ipos.x)
-            self.vel.x, self.vel.y = self.vel.y, -self.vel.x
- 
-        elif (self.face == Face.LEFT and self.ipos.x > self.dims):
-            self.face = Face.FRONT
-            self.pos = glm.vec2(0, self.ipos.y)
-        elif (self.face == Face.LEFT and self.ipos.x < 0):
-            self.face = Face.BACK
-            self.pos = glm.vec2(self.dims, self.ipos.y)
-        elif (self.face == Face.LEFT and self.ipos.y > self.dims):
-            self.face = Face.TOP
-            self.pos = glm.vec2(self.ipos.x, 0)
-        elif (self.face == Face.LEFT and self.ipos.y < 0):
-            self.face = Face.BOTTOM
-            self.pos = glm.vec2(self.ipos.x, self.dims)
-
-        elif (self.face == Face.RIGHT and self.ipos.x > self.dims):
-            self.face = Face.BACK
-            self.pos = glm.vec2(0, self.ipos.y)
-        elif (self.face == Face.RIGHT and self.ipos.x < 0):
-            self.face = Face.FRONT
-            self.pos = glm.vec2(self.dims, self.ipos.y)
-        elif (self.face == Face.RIGHT and self.ipos.y > self.dims):
-            self.face = Face.TOP
-            self.pos = glm.vec2(self.dims - self.ipos.x, self.dims)
-            self.vel = -self.vel
-        elif (self.face == Face.RIGHT and self.ipos.y < 0):
-            self.face = Face.BOTTOM
-            self.pos = glm.vec2(self.dims - self.ipos.x, 0)
-            self.vel = -self.vel
-
-        elif (self.face == Face.TOP and self.ipos.x > self.dims):
-            self.face = Face.FRONT
-            self.pos = glm.vec2(self.ipos.y, self.dims)
-            self.vel.x, self.vel.y = self.vel.y, -self.vel.x
-        elif (self.face == Face.TOP and self.ipos.x < 0):
-            self.face = Face.BACK
-            self.pos = glm.vec2(self.dims - self.ipos.y, self.dims)
-            self.vel.x, self.vel.y = -self.vel.y, self.vel.x
-        elif (self.face == Face.TOP and self.ipos.y > self.dims):
-            self.face = Face.RIGHT
-            self.pos = glm.vec2(self.dims - self.ipos.x, self.dims)
-            self.vel = -self.vel
-        elif (self.face == Face.TOP and self.ipos.y < 0):
-            self.face = Face.LEFT
-            self.pos = glm.vec2(self.ipos.x, self.dims)
-
-        elif (self.face == Face.BOTTOM and self.ipos.x > self.dims):
-            self.face = Face.FRONT
-            self.pos = glm.vec2(self.dims - self.ipos.y, 0)
-            self.vel.x, self.vel.y = -self.vel.y, self.vel.x
-        elif (self.face == Face.BOTTOM and self.ipos.x < 0):
-            self.face = Face.BACK
-            self.pos = glm.vec2(self.ipos.y, 0)
-            self.vel.x, self.vel.y = self.vel.y, -self.vel.x
-        elif (self.face == Face.BOTTOM and self.ipos.y > self.dims):
-            self.face = Face.LEFT
-            self.pos = glm.vec2(self.ipos.x, 0)
-        elif (self.face == Face.BOTTOM and self.ipos.y < 0):
-            self.face = Face.RIGHT
-            self.pos = glm.vec2(self.dims - self.ipos.x, 0)
-            self.vel = -self.vel
-
-        if add2list:
-            self.addNewPos(self.face, self.pos)
-            # if random.uniform(0, 9) > 8:
-            #     # self.vel.x, self.vel.y = self.vel.y, self.vel.x
-            #     rot = random.randint(-1, 1) * math.pi/2.0
-            #     t = glm.rotate(glm.mat4(1), rot, glm.vec3(0, 0, 1))
-            #     self.vel = (glm.vec4(self.vel, 0, 0) * t).xy
-
-        if self.selfIntersect():
-            self.__init__(self.dims+1)
+    def eat(self, food):
+        for f in food:
+            if self.face == f.face and self.ipos == f.ipos:
+                self.length += 1
+                food.remove(f)
 
     def left(self):
         t = glm.rotate(glm.mat4(1), math.pi/2, glm.vec3(0, 0, 1))
@@ -150,48 +67,159 @@ class Particle():
         t = glm.rotate(glm.mat4(1), -math.pi/2, glm.vec3(0, 0, 1))
         self.vel = (glm.vec4(self.vel, 0, 0) * t).xy
 
-    def selfIntersect(self):
-        return len(self.pos_list) != len(set(self.pos_list))
-
-    def addNewPos(self, face, pos):
-        pos = glm.ivec2(pos)
-        if len(self.pos_list) > 0:
-            f, p = self.pos_list[0]
-            if f != face or p != pos: # something is causing duplicates to be added to the front triggering self intersection
-                self.pos_list = [(face, pos)] + self.pos_list   #add new at front
-                if len(self.pos_list) > self.trail:
-                    self.pos_list = self.pos_list[:-1]      #remove last
-        else:
-            self.pos_list = [(face, pos)] + self.pos_list   #add new at front
+    def collide(self):
+        for c in self.tail[1:]: # the tail also contains the head
+            if self.face == c.face and self.ipos == c.ipos:
+                self.__init__(self.surface)
 
     def getPosition(self, i):
-        return self.pos_list[i]
+        return self.tail[i]
 
     def getColourFaded(self, i):
-        return fadeColour(self.col, 1 - (i / self.trail))
+        return fadeColour(self.col, 1 - (i / self.length))
+
+class EndlessCubeSurface():
+    def __init__(self, size):
+        self.dims = size - 1
+
+    def getRandomCell(self):
+        f = random.choice(list(Face))
+        p = glm.vec2(random.uniform(0, self.dims), random.uniform(0, self.dims))
+        return Cell(f, p)
+
+    def update(self, mcell, dt):
+        mcell.fpos += mcell.vel / dt
+
+        ipos = glm.ivec2(glm.floor(mcell.fpos))
+        movedCell = False
+        if ipos != mcell.ipos:
+            movedCell = True
+        mcell.ipos = ipos
+
+        if (mcell.face == Face.BACK and mcell.ipos.x > self.dims):
+            mcell.face = Face.LEFT
+            mcell.fpos = glm.vec2(0, mcell.ipos.y)
+        elif (mcell.face == Face.BACK and mcell.ipos.x < 0):
+            mcell.face = Face.RIGHT
+            mcell.fpos = glm.vec2(self.dims, mcell.ipos.y)
+        elif (mcell.face == Face.BACK and mcell.ipos.y > self.dims):
+            mcell.face = Face.TOP
+            mcell.fpos = glm.vec2(0, self.dims - mcell.ipos.x)
+            mcell.vel.x, mcell.vel.y = mcell.vel.y, -mcell.vel.x
+        elif (mcell.face == Face.BACK and mcell.ipos.y < 0):
+            mcell.face = Face.BOTTOM
+            mcell.fpos = glm.vec2(0, mcell.ipos.x)
+            mcell.vel.x, mcell.vel.y = -mcell.vel.y, mcell.vel.x
+
+        elif (mcell.face == Face.FRONT and mcell.ipos.x > self.dims):
+            mcell.face = Face.RIGHT
+            mcell.fpos = glm.vec2(0, mcell.ipos.y)
+        elif (mcell.face == Face.FRONT and mcell.ipos.x < 0):
+            mcell.face = Face.LEFT
+            mcell.fpos = glm.vec2(self.dims, mcell.ipos.y)
+        elif (mcell.face == Face.FRONT and mcell.ipos.y > self.dims):
+            mcell.face = Face.TOP
+            mcell.fpos = glm.vec2(self.dims, mcell.ipos.x)
+            mcell.vel.x, mcell.vel.y = -mcell.vel.y, mcell.vel.x
+        elif (mcell.face == Face.FRONT and mcell.ipos.y < 0):
+            mcell.face = Face.BOTTOM
+            mcell.fpos = glm.vec2(self.dims, self.dims - mcell.ipos.x)
+            mcell.vel.x, mcell.vel.y = mcell.vel.y, -mcell.vel.x
+ 
+        elif (mcell.face == Face.LEFT and mcell.ipos.x > self.dims):
+            mcell.face = Face.FRONT
+            mcell.fpos = glm.vec2(0, mcell.ipos.y)
+        elif (mcell.face == Face.LEFT and mcell.ipos.x < 0):
+            mcell.face = Face.BACK
+            mcell.fpos = glm.vec2(self.dims, mcell.ipos.y)
+        elif (mcell.face == Face.LEFT and mcell.ipos.y > self.dims):
+            mcell.face = Face.TOP
+            mcell.fpos = glm.vec2(mcell.ipos.x, 0)
+        elif (mcell.face == Face.LEFT and mcell.ipos.y < 0):
+            mcell.face = Face.BOTTOM
+            mcell.fpos = glm.vec2(mcell.ipos.x, self.dims)
+
+        elif (mcell.face == Face.RIGHT and mcell.ipos.x > self.dims):
+            mcell.face = Face.BACK
+            mcell.fpos = glm.vec2(0, mcell.ipos.y)
+        elif (mcell.face == Face.RIGHT and mcell.ipos.x < 0):
+            mcell.face = Face.FRONT
+            mcell.fpos = glm.vec2(self.dims, mcell.ipos.y)
+        elif (mcell.face == Face.RIGHT and mcell.ipos.y > self.dims):
+            mcell.face = Face.TOP
+            mcell.fpos = glm.vec2(self.dims - mcell.ipos.x, self.dims)
+            mcell.vel = -mcell.vel
+        elif (mcell.face == Face.RIGHT and mcell.ipos.y < 0):
+            mcell.face = Face.BOTTOM
+            mcell.fpos = glm.vec2(self.dims - mcell.ipos.x, 0)
+            mcell.vel = -mcell.vel
+
+        elif (mcell.face == Face.TOP and mcell.ipos.x > self.dims):
+            mcell.face = Face.FRONT
+            mcell.fpos = glm.vec2(mcell.ipos.y, self.dims)
+            mcell.vel.x, mcell.vel.y = mcell.vel.y, -mcell.vel.x
+        elif (mcell.face == Face.TOP and mcell.ipos.x < 0):
+            mcell.face = Face.BACK
+            mcell.fpos = glm.vec2(self.dims - mcell.ipos.y, self.dims)
+            mcell.vel.x, mcell.vel.y = -mcell.vel.y, mcell.vel.x
+        elif (mcell.face == Face.TOP and mcell.ipos.y > self.dims):
+            mcell.face = Face.RIGHT
+            mcell.fpos = glm.vec2(self.dims - mcell.ipos.x, self.dims)
+            mcell.vel = -mcell.vel
+        elif (mcell.face == Face.TOP and mcell.ipos.y < 0):
+            mcell.face = Face.LEFT
+            mcell.fpos = glm.vec2(mcell.ipos.x, self.dims)
+
+        elif (mcell.face == Face.BOTTOM and mcell.ipos.x > self.dims):
+            mcell.face = Face.FRONT
+            mcell.fpos = glm.vec2(self.dims - mcell.ipos.y, 0)
+            mcell.vel.x, mcell.vel.y = -mcell.vel.y, mcell.vel.x
+        elif (mcell.face == Face.BOTTOM and mcell.ipos.x < 0):
+            mcell.face = Face.BACK
+            mcell.fpos = glm.vec2(mcell.ipos.y, 0)
+            mcell.vel.x, mcell.vel.y = mcell.vel.y, -mcell.vel.x
+        elif (mcell.face == Face.BOTTOM and mcell.ipos.y > self.dims):
+            mcell.face = Face.LEFT
+            mcell.fpos = glm.vec2(mcell.ipos.x, 0)
+        elif (mcell.face == Face.BOTTOM and mcell.ipos.y < 0):
+            mcell.face = Face.RIGHT
+            mcell.fpos = glm.vec2(self.dims - mcell.ipos.x, 0)
+            mcell.vel = -mcell.vel
+
+        return movedCell
 
 class EndlessSurface(Application):
     def __init__(self):
         super().__init__(1280, 720, 60, 64)
 
-        self.particles = []
-        for i in range(50):
-            self.particles.append(Particle(self.led_cube.size))
+        self.surface = EndlessCubeSurface(self.led_cube.size)
+        self.snakes = []
+        self.food = []
+        for i in range(1):
+            self.snakes.append(Snake(self.surface))
+        for i in range(100):
+            self.food.append(self.surface.getRandomCell())
 
     def handleEvent(self, event):
         if (event.type == pg.KEYDOWN and event.key == pg.K_a):
-            self.particles[0].left() 
+            self.snakes[0].left() 
         if (event.type == pg.KEYDOWN and event.key == pg.K_d):
-            self.particles[0].right()
+            self.snakes[0].right()
 
     def update(self, dt):
         cube_faces = self.led_cube.getCubeArrayAsColour([0.1, 0.1, 0.1])
 
-        for p in self.particles:
-            p.update(dt)
-            for i in range(len(p.pos_list)):
-                face_id, coords = p.getPosition(i)
-                cube_faces[face_id, coords.x, coords.y] = p.getColourFaded(i)
+        for f in self.food:
+            cube_faces[f.face, f.ipos.x, f.ipos.y] = [1, 1, 1]
+
+        for s in self.snakes:
+            if self.surface.update(s, dt):
+                s.step()
+            s.collide()
+            s.eat(self.food)
+            for i in range(len(s.tail)):
+                cell = s.getPosition(i)
+                cube_faces[cell.face, cell.ipos.x, cell.ipos.y] = s.getColourFaded(i)
 
         self.led_cube.updateFaces(cube_faces)
         self.led_cube.update()
